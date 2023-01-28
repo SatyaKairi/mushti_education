82d8a60691d76b65d2bf9268252f8a0e01b75c39
MacGitHub2013 <bhanu.net10@gmail.com>
2023-01-28 10:34:31 +0530

    subject detail view

--- mushti/Subject/views.py
+++ mushti/Subject/views.py
@@ -7,28 +7,31 @@ from django.db.models import Q
 from .forms import *
 # Create your views here.
 
-def Subject_create_update_view(request,*args, **kwargs):
-    Subject=None
+
+def Subject_create_update_view(request, *args, **kwargs):
+    Subject = None
     msg = "Create Subject"
     Subject_id = kwargs.get('id')
     if not Subject_id is None:
         Subject = Subject.objects.get(id=Subject_id)
         msg = "Update Subject"
 
-    form = SubjectCreateForm(request.POST or None,instance=Subject)
+    form = SubjectCreateForm(request.POST or None, instance=Subject)
     if request.POST:
         if form.is_valid():
-            Subject =form.instance
+            Subject = form.instance
             Subject.user = request.user
             form.save()
-            return redirect (form.instance.get_detail_url())             
+            return redirect(form.instance.get_detail_url())
     context = {
-            'form':form,
-            'object':Subject,
-            'msg':msg
+        'form': form,
+        'object': Subject,
+        'msg': msg
 
-        }      
-    return render(request,'Subject/create.html',context)   
+    }
+    return render(request, 'Subject/create.html', context)
+
+
 def Subject_deactivate_view(request):
     if request.POST:
         id = request.POST.get('Subject_id')
@@ -38,24 +41,25 @@ def Subject_deactivate_view(request):
         Subject.save()
         return redirect('Subject:list_view')
 
-def Subject_detail_view(request,id,*args, **kwargs):
-    subject = get_object_or_404(Subject,id=id,is_active = True)
- 
-    context ={
-        'object':subject,
+
+def Subject_detail_view(request, id, *args, **kwargs):
+    subject = get_object_or_404(Subject, id=id, is_active=True)
+
+    context = {
+        'object': subject,
     }
-    
-    return render(request,'Subject/detail.html',context)
+
+    return render(request, 'Subject/detail_all.html', context)
 
 
-def Subject_list_view(request,*args, **kwargs):
-    q= request.GET.get('q')
+def Subject_list_view(request, *args, **kwargs):
+    q = request.GET.get('q')
     qs = Subject.objects.get_all_active_Subjects()
     if not q is None:
-  #TODO: add search Parameters below:
-        search_param =  Q(id__icontains =q )| Q( name__icontains = q)
+      # TODO: add search Parameters below:
+        search_param = Q(id__icontains=q) | Q(name__icontains=q)
         qs = qs.filter(search_param)
-    context ={
+    context = {
         'object_list': qs
     }
-    return render(request,'Subject/list.html',context)
+    return render(request, 'Subject/list.html', context)
